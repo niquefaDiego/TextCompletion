42fbc87bfc36ed5f225b667d62a519ca405da6bd
niquefaDiego <niquefa.diego@gmail.com>
2017-11-19 16:36:51 -0500

    bind text area size to scene size and change default window size

--- src/application/WriterApplication.java
+++ src/application/WriterApplication.java
@@ -5,14 +5,21 @@ import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class WriterApplication extends Application{
+public class WriterApplication extends Application
+{
 	
 	@Override
     public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle(":) :) :)");
-		AutocompleteTextArea textField = new AutocompleteTextArea();
-        VBox vbox = new VBox(textField);
-		Scene scene = new Scene( vbox, 300, 500 );
+		primaryStage.setTitle("Predictor");
+		AutocompleteTextArea textArea = new AutocompleteTextArea();
+		
+        VBox vbox = new VBox(textArea);
+		Scene scene = new Scene( vbox, 600, 500 );
+		vbox.prefHeightProperty().bind(scene.heightProperty());
+		vbox.prefWidthProperty().bind(scene.widthProperty());
+		textArea.prefWidthProperty().bind(vbox.prefWidthProperty());
+		textArea.prefHeightProperty().bind(vbox.prefHeightProperty());
+
 		
 		primaryStage.setScene(scene);
 		primaryStage.show();
